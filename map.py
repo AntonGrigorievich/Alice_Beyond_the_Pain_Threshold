d87d90d135ac7f9198ecc0b00803e7179d83f8c4
husky <gorbatenko_1910@list.ru>
2023-01-08 21:05:56 +0500

    hero and mob sprites draw

--- map.py
+++ map.py
@@ -11,8 +11,6 @@ class Map:
     def render(self, group):
         for y in range(self.height):
             for x in range(self.width):
-                # image = self.map.get_tile_image(x, y, 0)
-                # screen.blit(image, (x * self.tilesize, y * self.tilesize))
                 tile = pygame.sprite.Sprite(group)
                 tile.image = self.map.get_tile_image(x, y, 0)
                 tile.rect = tile.image.get_rect()
