0fdde18a390c7883278de3aab879257c05387688
husky <gorbatenko_1910@list.ru>
2023-01-12 21:07:31 +0500

    tiled fix 2

--- data/maps/tile_bilboards.tsx
+++ data/maps/tile_bilboards.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="tile_bilboards" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
- <image source="../../../tiles_bilboards.png" width="512" height="512"/>
+ <image source="../tiles/tiles_bilboards.png" width="512" height="512"/>
  <wangsets>
   <wangset name="Unnamed Set" type="mixed" tile="-1">
    <wangcolor name="purple wall" color="#ff0000" tile="-1" probability="1"/>
